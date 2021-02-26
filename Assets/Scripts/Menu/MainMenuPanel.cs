abb745b3da3c257f5bd6142703b4f00e2a675350
Mason Reed <mchandlerreed@gmail.com>
2021-02-26 13:49:35 -0800

    Level edits; Splash Screen Update

--- Assets/Scripts/Menu/MainMenuPanel.cs
+++ Assets/Scripts/Menu/MainMenuPanel.cs
@@ -7,24 +7,14 @@ namespace DJ2
 {
     public class MainMenuPanel : MonoBehaviour
     {
-        private static bool FromLevel = false;
 
-        [SerializeField] private GameObject m_levelSelect;
-
-        private void Awake()
+        private void Update()
         {
-            if (MainMenuPanel.FromLevel)
+            if (Input.GetKeyDown(KeyCode.Space))
             {
-                GoToLevelSelect();
+                SceneManager.LoadScene("LevelSelect");
             }
         }
 
-        public void GoToLevelSelect()
-        {
-            // SceneManager.LoadScene("LevelSelect");
-            MainMenuPanel.FromLevel = true;
-            gameObject.SetActive(false);
-            m_levelSelect.SetActive(true);
-        }
     }
 }
